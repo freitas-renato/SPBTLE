ee5c1bda44d5365863aa0f541d7f58441ee62099
freitas-renato <renatinho008@hotmail.com>
2019-03-31 13:31:35 -0300

    :arrow_up: Update to X-CUBE-BLE1.4.3.0 package

--- lib/spbtle/sources.mk
+++ lib/spbtle/sources.mk
@@ -1,10 +1,10 @@
 THIS_PATH := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 
-C_INCLUDES +=                                                          \
-	-I$(CUBE_DIR)/Middlewares/ST/BlueNRG-MS/includes                   \
-	-I$(CUBE_DIR)/Middlewares/ST/BlueNRG-MS/hci/hci_tl_patterns/Basic  \
-	-I$(CUBE_DIR)/Middlewares/ST/BlueNRG-MS/utils                      \
-	-I$(THIS_PATH)/inc                                                 \
+C_INCLUDES +=                                                                      \
+	-I$(CUBE_DIR)/Middlewares/ST/X-CUBE-BLE1_BlueNRG-MS/includes                    \
+	-I$(CUBE_DIR)/Middlewares/ST/X-CUBE-BLE1_BlueNRG-MS/hci/hci_tl_patterns/Basic   \
+	-I$(CUBE_DIR)/Middlewares/ST/X-CUBE-BLE1_BlueNRG-MS/utils                       \
+	-I$(THIS_PATH)/inc                                                              \
 
 SUBM_SOURCES += $(shell find $(THIS_PATH) -name "*.c")
 
